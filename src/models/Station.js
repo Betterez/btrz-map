ed2854f7e623c188873b9bf6b9fcd56d86cf8699
Marcos Tomasello <marcos.tomasello@gmail.com>
2021-11-29 11:59:29 -0300

    #1r041cw change icon station icon sizes on zoom change

--- src/models/Station.js
+++ src/models/Station.js
@@ -1,6 +1,4 @@
 import L from "leaflet";
-import LeafIcon from '../images/leaf-green.png';
-import LeafShadowIcon from '../images/leaf-shadow.png';
 import origin from '../images/origin.png';
 import origin2x from '../images/origin-2x.png';
 import station from '../images/station.png';
@@ -8,47 +6,71 @@ import station2x from '../images/station-2x.png';
 import destination from '../images/destination.png';
 import destination2x from '../images/destination-2x.png';
 
-const stationIcon = L.icon({
-  iconUrl: station,
-  iconSize:     [36, 49],
-  iconAnchor:   [18, 47],
-  popupAnchor:  [-3, -56] 
+const StationIcon = L.Icon.extend({
+  options: {
+    iconSize:     [36, 49],
+    iconAnchor:   [18, 47],
+    popupAnchor:  [-3, -56]
+  }
 });
 
-const firstStationIcon = L.icon({
-  iconUrl: origin,
-  iconSize:     [36, 49],
-  iconAnchor:   [18, 47],
-  popupAnchor:  [-3, -56] 
-});
+const stationIcon = new StationIcon({iconUrl: station});
+const firstStationIcon = new StationIcon({iconUrl: origin});
+const lastStationIcon = new StationIcon({iconUrl: destination});
 
-const lastStationIcon = L.icon({
-  iconUrl: destination,
-  iconSize:     [36, 49],
-  iconAnchor:   [18, 47],
-  popupAnchor:  [-3, -56] 
-});
+const stationIcon2x = new StationIcon({iconUrl: station2x});
+const firstStationIcon2x = new StationIcon({iconUrl: origin2x});
+const lastStationIcon2x = new StationIcon({iconUrl: destination2x});
+
 
 export class Station {
   constructor(stationData, indexInTrip, isLastStation) {
+    console.log("Station constructor")
     this.id = stationData._id;
     this.name = stationData.name;
     this.latitude = stationData.latitude;
     this.longitude = stationData.longitude;
+    this.currentZoom = 0;
+    this.isLastStation = isLastStation;
+    this.indexInTrip = indexInTrip;
 
-    if (isLastStation) {
-      this.marker = L.marker([stationData.latitude, stationData.longitude], {icon: lastStationIcon});
-      this.marker.bindPopup(`<b>${this.name}</b><br>ETA ${this.arrival}`);
-    } else if (indexInTrip === 0) {
-      this.marker = L.marker([stationData.latitude, stationData.longitude], {icon: firstStationIcon});
+    const initialIcon = this._getIcon("normal");
+    this.marker = L.marker([stationData.latitude, stationData.longitude], {icon: initialIcon});
+
+    if (indexInTrip === 0) {
       this.marker.bindPopup(`<b>${this.name}</b><br>ETD ${this.departure}`);
     } else {
-      this.marker = L.marker([stationData.latitude, stationData.longitude], {icon: stationIcon});
       this.marker.bindPopup(`<b>${this.name}</b><br>ETA ${this.arrival}`);
     }
   }
 
+  _getIcon(size) {
+    if (this.isLastStation) {
+      return size === "normal" ? lastStationIcon : lastStationIcon2x;
+    } else if (this.indexInTrip === 0) {
+      return size === "normal" ? firstStationIcon : firstStationIcon2x;
+    } else {
+      return size === "normal" ? stationIcon : stationIcon2x;
+    }
+  }
+
+  _adjustIcon(zoom) {
+    if (zoom < 9 && this.currentZoom >= 9) {
+      this.marker.setIcon(this._getIcon("2x"));
+    } else if (zoom >= 9 && this.currentZoom < 9) {
+      this.marker.setIcon(this._getIcon("normal"));
+    }
+
+    this.currentZoom = zoom;
+  }
+
   addTo(map) {
+    map.on('zoomend', () => {
+      const newZoom = map.getZoom();
+      this._adjustIcon(newZoom);
+      console.log("currentZoom: ", newZoom);
+    });
+
     this.marker.addTo(map);
   }
 
