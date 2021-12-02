ff5c411102834ce2897d309b860d15269c19cfed
Marcos Tomasello <marcos.tomasello@gmail.com>
2021-12-02 15:12:32 -0300

    #1r041cw transpiling for web

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  target: ['web', 'es5'],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "btrz-map.js",
@@ -28,6 +29,16 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"]
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
 };
