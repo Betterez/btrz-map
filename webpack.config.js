6cf8e530d5ea40e1658fa71c5c2a5c0b5298ad0f
Marcos Tomasello <marcos.tomasello@gmail.com>
2021-12-02 17:42:24 -0300

    #1r041cw fix for server-side imports

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "btrz-map.js",
+    globalObject: "this",
     library: {
       name: "btrzMap",
       type: "umd"
